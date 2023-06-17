2059df3ff810cbe509f1f144e6d7a76d6010630c
Pavel <black_yaguar@mail.ru>
2023-06-17 11:15:04 +0500

    Создание папкм с файлом Toys, создание класса Toys

---
+++ RaffleToys/Toys/Toys.java
@@ -0,0 +1,47 @@
+package RaffleToys.Toys;
+
+public class Toys {
+    private String id ="";
+    private String toysName;
+    private Integer quantity;
+    private Integer weight;
+
+    public Toys( String toysName, Integer quantity, Integer weight){
+        this.toysName = toysName;
+        this.quantity = quantity;
+        this.weight = weight;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setToysName(String toysName) {
+        this.toysName = toysName;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public void setWeight(Integer weight) {
+        this.weight = weight;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getToysName() {
+        return toysName;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public Integer getWeight() {
+        return weight;
+    }
+    
+}
