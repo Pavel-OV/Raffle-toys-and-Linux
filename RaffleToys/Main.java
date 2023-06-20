303fb869421e0aa5ccf89d1ea5a2cfd06dd5ac99
Pavel <black_yaguar@mail.ru>
2023-06-20 12:20:32 +0500

    Изменеие построения структуры самого магазина

--- RaffleToys/Main.java
+++ RaffleToys/Main.java
@@ -1,33 +1,24 @@
 
 package RaffleToys;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
 import java.util.Scanner;
+
+import RaffleToys.Toys.ToyStore;
+import RaffleToys.Toys.Toys;
 import RaffleToys.views.ViewUser;
 
 public class Main {
     public static void main(String[] args) {
+        ToyStore mart = new ToyStore();
         System.out.println("Это магазин игрушек. Можно просмотреть список игрушек, некоторые параметры - вес краткое описание ");  
         System.out.println("Добавить игрушку, изменить вес  Выберете действие");
-        ViewUser view = new ViewUser();
-        view.run();
-
-        // Scanner scanner = new Scanner(System.in);
-        // int a = scanner.nextInt();
-        // if (a == 1) {
-        //     System.out.printf("hghgh", a);
-
-        // }
-        // if (a == 2) {
-        //     System.out.printf("FFFFFF", a);
-        // }
-        // switch (a){
-        //     case 1:
-        //     System.out.printf("QQQQQQQQQQQQQQQQQQQ",a);
-        //     break;
-        //     case 2:
-        //     System.out.println("dfghjkhgfgh");
-        // }
+       
+        mart.addToys(new Toys(1,"Баба Яга", "3", "150"))
+            .addToys(new Toys(2, "Змей Горыныч", "5" ,"300"));
+        System.out.println(mart.toString());
     }
+        
 }
