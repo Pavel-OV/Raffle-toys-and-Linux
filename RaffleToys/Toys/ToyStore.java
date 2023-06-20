952258864e6e79c6ffa0377285b8a28ae5daf9c3
Pavel <black_yaguar@mail.ru>
2023-06-21 00:31:00 +0500

    Класс магазин игрушек, создание функции addToys, editToysWeight

--- RaffleToys/Toys/ToyStore.java
+++ RaffleToys/Toys/ToyStore.java
@@ -4,28 +4,55 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ToyStore {
-    private List<Toys> toys = new ArrayList<>();
+    private List<Toys> toy = new ArrayList<>();
+    
+     public ToyStore addToys(Toys toys  ){
+        toy.add(toys);
+        return this;
+     }
     
 
    
 
-    public ToyStore addToys(Toys toy){
-        toys.add(toy);
-        return this;
+   
+    // public boolean getWeight() {
+    //     return false;
+    // }
+
+    // public boolean getQuantity() {
+    //     return false;
+    // }
+    public Toys searchToys(String stoysName) {
+       
+       for (Toys item : toy) {
+           if (item.getToysName().equals(stoysName)) {
+               return item;
+           }
+       }
+       return null;
+
+   }
+   public void editToysWeight(int id, int weight){
+    for(Toys toys : toy){
+        if (toys.getId()==id){
+            toys.setWeight(weight);
+        return;
+        }
+        System.out.println("id"+id+"no");
     }
 
-    public boolean getWeight() {
-        return false;
-    }
 
-    public boolean getQuantity() {
-        return false;
-    }
+  
+
+   }
+
+    
+
 
     @Override
     public String toString() {
         StringBuilder lst =new StringBuilder();
-        for (Toys item : toys ){
+        for (Toys item : toy ){
             lst.append(item);
             lst.append("\n");
         }
