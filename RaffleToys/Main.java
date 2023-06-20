64afc560f678ab0084dc3d04b7536707db542ebc
Pavel <black_yaguar@mail.ru>
2023-06-20 12:49:28 +0500

    Добавил игрушки

--- RaffleToys/Main.java
+++ RaffleToys/Main.java
@@ -1,14 +1,10 @@
 
 package RaffleToys;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.List;
-import java.util.Scanner;
+
 
 import RaffleToys.Toys.ToyStore;
 import RaffleToys.Toys.Toys;
-import RaffleToys.views.ViewUser;
+
 
 public class Main {
     public static void main(String[] args) {
@@ -16,8 +12,14 @@ public class Main {
         System.out.println("Это магазин игрушек. Можно просмотреть список игрушек, некоторые параметры - вес краткое описание ");  
         System.out.println("Добавить игрушку, изменить вес  Выберете действие");
        
-        mart.addToys(new Toys(1,"Баба Яга", "3", "150"))
-            .addToys(new Toys(2, "Змей Горыныч", "5" ,"300"));
+        mart.addToys(new Toys(1,"Баба Яга", 3, 150))
+            .addToys(new Toys(2, "Змей Горыныч", 5 ,300))
+            .addToys(new Toys(3,"Мяч", 2, 150))
+            .addToys(new Toys(4,"Домовой", 1, 150))
+            .addToys(new Toys(5,"Баба Яга", 3, 150))
+            .addToys(new Toys(6,"Баба Яга", 3, 150))
+            .addToys(new Toys(7,"Баба Яга", 3, 150))
+            .addToys(new Toys(8,"Баба Яга", 3, 150));
         System.out.println(mart.toString());
     }
         
