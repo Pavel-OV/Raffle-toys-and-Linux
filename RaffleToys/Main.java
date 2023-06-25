5c1149ce0ee8507ae895bd8a46575e9e6cf20601
Pavel <black_yaguar@mail.ru>
2023-06-25 13:53:54 +0500

    Исправлена орфографическая ошибка

--- RaffleToys/Main.java
+++ RaffleToys/Main.java
@@ -7,11 +7,11 @@ import java.util.List;
 import RaffleToys.Toys.ToyStore;
 import RaffleToys.Toys.Toys;
 
-
 public class Main {
+     
     public static void main(String[] args) {
-        ToyStore mart = new ToyStore();
-        mart.addToys(new Toys(1, "Баба Яга", 3, 150))
+        ToyStore mark = new ToyStore();
+        mark.addToys(new Toys(1, "Баба Яга", 3, 150))
                 .addToys(new Toys(2, "Змей Горыныч", 5, 300))
                 .addToys(new Toys(3, "Корабль", 2, 150))
                 .addToys(new Toys(4, "Домовой", 1, 150))
@@ -23,22 +23,22 @@ public class Main {
         // System.out.println(mart.toString());    
        
 
-        mart.editToysWeight(5, 100);
-        mart.editToysWeight(2, 300);
-        mart.editToysWeight(3, 350);
-        mart.editToysWeight(4, 100);
-        mart.editToysWeight(5, 200);
-        mart.editToysWeight(6, 180);
-        mart.editToysWeight(7, 210);
-        mart.editToysWeight(8, 85);
+        mark.editToysWeight(5, 100);
+        mark.editToysWeight(2, 300);
+        mark.editToysWeight(3, 350);
+        mark.editToysWeight(4, 100);
+        mark.editToysWeight(5, 200);
+        mark.editToysWeight(6, 180);
+        mark.editToysWeight(7, 210);
+        mark.editToysWeight(8, 85);
         System.out.println("Список игрушек после изменения в веса");        
-        System.out.println(mart.toString());
+        System.out.println(mark.toString());
         
         // организация розыгрыша и получение призовых игрушек
 
         List<Toys> prizeToys = new ArrayList<>();
         for (int i = 0; i < 5; i++) {
-            Toys prizeToy = mart.drawingPrizeToys();
+            Toys prizeToy = mark.drawingPrizeToys();
             if (prizeToy != null) {
                 prizeToys.add(prizeToy);
             }
@@ -48,9 +48,9 @@ public class Main {
         System.out.println("\nСписок призовых игрушек: \n");
         for (Toys toy : prizeToys) {
             System.out.println(toy.getToysName());
-        }
-
-       
+        }       
 
     }
+
+   
 }
